e0463cf44ee530e06e12d6697d6ae3147cbbc94e
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2019-07-11 21:31:42 -0700

    project created

---
+++ src/main/java/com/springboot/restfulapi/springbootrestfulapi/SpringBootRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restfulapi.springbootrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulApiApplication.class, args);
+	}
+
+}
