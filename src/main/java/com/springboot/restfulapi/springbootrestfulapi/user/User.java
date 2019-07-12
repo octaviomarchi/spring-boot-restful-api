19af7b39c0ca8965657dff3cbb6a96361c5d9336
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2019-07-11 22:51:38 -0700

    POST Method to create a User Resource

--- src/main/java/com/springboot/restfulapi/springbootrestfulapi/user/User.java
+++ src/main/java/com/springboot/restfulapi/springbootrestfulapi/user/User.java
@@ -9,6 +9,10 @@ public class User {
 	private String name;
 
 	private Date birthDate;
+	
+	protected User() {
+		
+	}
 
 	public User(Integer id, String name, Date birthDate) {
 		super();
--- src/main/java/com/springboot/restfulapi/springbootrestfulapi/user/UserResource.java
+++ src/main/java/com/springboot/restfulapi/springbootrestfulapi/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,14 +14,20 @@ public class UserResource {
 
 	@Autowired
 	private UserDaoService userDaoService;
-	
+
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 		return userDaoService.findAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return userDaoService.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		User savedUser = userDaoService.save(user);
+		return savedUser;
 	}
 }
