7f1db9852cff61b00d63bd9b3a832ab708546566
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2019-07-11 23:01:29 -0700

    Enhancing POST Method
    returning correct HTTP Status Code and Location

--- src/main/java/com/springboot/restfulapi/springbootrestfulapi/user/UserResource.java
+++ src/main/java/com/springboot/restfulapi/springbootrestfulapi/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.springboot.restfulapi.springbootrestfulapi.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -24,10 +27,14 @@ public class UserResource {
 	public User retrieveUser(@PathVariable int id) {
 		return userDaoService.findOne(id);
 	}
-	
+
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = userDaoService.save(user);
-		return savedUser;
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+		
 	}
 }
